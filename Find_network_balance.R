7459d5a75c79f71fe096440ac5f229ab85d7127e
Jonathan Bourne <cheg_a_leg@hotmail.com>
2019-06-05 17:52:59 +0100

    Two node solution
    This change finds a two node solution for the network
    this is faster than the dynamic version

--- Find_network_balance.R
+++ Find_network_balance.R
@@ -1,8 +1,10 @@
-Find_network_balance <- function(g, force ="BalencedPower", flow = "PowerFlow", capacity = "Link.Limit",  tstep = 0.5, distance = "Imp", mass = 2000, maxIter =2000, frctmultiplier = 1, tol = 1e-10, verbose = TRUE){
+Find_network_balance <- function(g, force ="BalencedPower", flow = "PowerFlow", capacity = "Link.Limit",  tstep = 0.5, distance = "Imp", 
+                                 mass = 2000, maxIter =2000, frctmultiplier = 1, tol = 1e-10, verbose = TRUE,
+                                 TwoNodeSolution = TRUE){
   #needs an edge attribute "distance"
-
+  
   g <- set.edge.attribute(g, "distance", value = get.edge.attribute(g, distance))
-
+  
   A <- as_data_frame(g) %>% 
     select(Link, from, to) %>% 
     gather(key = type, Node, -Link) %>%
@@ -33,18 +35,46 @@ Find_network_balance <- function(g, force ="BalencedPower", flow = "PowerFlow", 
       NetForce = force + NetTension - friction,
       acceleration = NetForce/mass,
       t = 0)
-
+  
   Link <- as_data_frame(g)  %>%
     rename(flow = flow,
            capacity = capacity) %>%
     mutate(EdgeName = Link, LL = abs(flow)/capacity, k = Area*E/distance) %>% #This sets a floor and ceiling 
-    #to the k values. the more highly loaded a line is the more it should stretch. as LL varies between 0, no loading (stiffest)
+    #to the k values. the more highly loaded a line is the more it should stretch. as LL varies between 0, no loading (stiffness)
     #to 1, overload point, (most elastic). The larger kdiff is the larger the difference in elasticity for highly and lightly loaded lines.
     #Very large kdiff means very little elasticty on lightly loaded lines
     select(EdgeName, distance, k) 
   
-  test <-FindStabilSystem2(NodeStatus, A, Link$k, Link$distance, tstep, maxIter, frctmultiplier, tol, verbose = verbose) 
+  #do special case solution
+  if(nrow(Link)==1 & TwoNodeSolution){
+
+    #uses the non-linear optimiser from minpack.lm to find the solution to the two node special case, this is much faster
+    solution_angle <- nlsLM(Force ~ ForceV_from_angle(target_angle, k = k, d = d), 
+                  start = c(target_angle = pi/4), 
+                  data = list(Force = abs(NodeStatus$force[1]), k = Link$k, d = Link$distance), 
+                  upper = pi/2)
+    
+    temp <- NodeStatus %>%
+      mutate(z = ifelse(force>0, 
+                        tan(coefficients(solution_angle))/2, #height above mid point
+                        -tan(coefficients(solution_angle))/2 ),
+             acceleration = 0,
+             t = 0,
+             Delta_acceleration = 0) #height below mid-point
+    
+    Out <- list(results = temp, 
+                NodeList = temp
+                  )
+    
+    
+    
+  } else{
+
+    Out <- FindStabilSystem2(NodeStatus, A, Link$k, Link$distance, tstep, maxIter, frctmultiplier, tol, verbose = verbose) 
+    
+  }
+
   
-  return(test)
+  return(Out)
   
 }
