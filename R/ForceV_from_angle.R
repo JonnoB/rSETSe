3d5fbfd54f7c4199205a2dc33b952dd1cbaf7e19
Jonathan Bourne <cheg_a_leg@hotmail.com>
2020-07-03 19:11:43 +0100

    Speed updates
    Major speed improvements for fix_z_to_origin and ForceV_from_angle. Both were stripped back to base R and vectors where possible. speed up in the range of 9x faster

--- R/ForceV_from_angle.R
+++ R/ForceV_from_angle.R
@@ -9,9 +9,15 @@
 
 ForceV_from_angle <- function(target_angle = 5*pi/12, k, d=temp$d){
   
-  tibble(H = sqrt(d^2 * (1 + tan(target_angle)^2)),
-         ForceT = k*(H-d), 
-         ForceV = ForceT*sin(target_angle)) %>%
-    pull(ForceV)
+  H = sqrt(d^2 * (1 + tan(target_angle)^2))
+  ForceT = k*(H-d)
+  ForceV = ForceT*sin(target_angle)
+  #IT is reallt slow to use a tibble here keeping everything thing base R is much faster
+  # tibble(H = sqrt(d^2 * (1 + tan(target_angle)^2)),
+  #        ForceT = k*(H-d), 
+  #        ForceV = ForceT*sin(target_angle)) %>%
+  #   pull(ForceV)
+  
+  return(ForceV)
   
 }
--- R/fix_z_to_origin.R
+++ R/fix_z_to_origin.R
@@ -11,21 +11,24 @@
 #' @param ArticulationVect The articulation nodes of the network.
 
 fix_z_to_origin <- function(relative_blocks, ArticulationVect){
-
+  
   #This function needs some further explaining!
   
   target_blocks <- 0
-  absolute_blocks <- relative_blocks %>% filter(Reference_ID ==-1)
-  Articulation_df <- relative_blocks %>% filter(Reference_ID ==-1)
+  absolute_blocks <- Articulation_df <- relative_blocks[relative_blocks$Reference_ID ==-1,]
   
   #This vector stors articulation nodes that have already been used,
   #This stops the same node being recycled which causes errors
   #The vector begins as an empty vector
   used_articulation_nodes<- as.numeric(list())
   
-  next_group <- relative_blocks %>% 
-    filter(Reference_ID %in% target_blocks) %>%
-    mutate(elevation = elevation ) #can this be removed? It doesn't seem to do anything
+  next_group <- relative_blocks[relative_blocks$Reference_ID %in% target_blocks,]
+  
+  
+  # vectors that are used
+  # reference_id <- relative_blocks$Reference_ID
+  # node_vect <- relative_blocks$node
+  # articulation_df <- relative_blocks$Articulation_node
   
   #The previous while used the rows in Articulation_df but this changed and so could be confusing. As there can only be the same 
   #amount of loops as there are articulation nodes. n must be equal to or smaller than the number of articulation nodes - 1
@@ -33,48 +36,44 @@ fix_z_to_origin <- function(relative_blocks, ArticulationVect){
   #while(n <= (length(ArticulationVect)-2)){
   
   while(length(used_articulation_nodes)< length(ArticulationVect)){
+    #  while(Art_n != 190){
     
     #add new absolute references to the dataframe
-    absolute_blocks <- next_group %>%
-      bind_rows(absolute_blocks,.) #this should be pre-allocated
+    absolute_blocks <- bind_rows(absolute_blocks, next_group)
     
     #add new articulation nodes to the dataframe
-    Articulation_df <- next_group %>%
-      filter(Articulation_node) %>%
-      bind_rows(Articulation_df,.) %>%
-      filter(!(node %in% used_articulation_nodes)) #remove all previously used articulation nodes
+    {
+      Articulation_df <- bind_rows(Articulation_df, next_group[next_group$Articulation_node,] )
+      Articulation_df <-Articulation_df[!(Articulation_df$node %in% used_articulation_nodes),]  #remove all previously used articulation nodes
+    }
+    
     #The removal of previously used articulation nodes is important as all articulation nodes are in the network at least twice
     
     #remove all new absolute references from the dataframe
-    relative_blocks <- relative_blocks %>%
-      filter(!(Reference_ID %in% target_blocks))
+    relative_blocks <- relative_blocks[!(relative_blocks$Reference_ID %in% target_blocks),] 
     
     #get the next articulation node in the queue
     Art_n <-Articulation_df$node[1] #
-    
+    #print(Art_n)
     #subtract art_n relative from all elevation scores
     #ass art_n abs to all values
     
     #This has to be done by block not all blocks together
-    target_blocks <- relative_blocks %>%
-      filter(node == Art_n) %>% pull(Reference_ID)
+    target_blocks <- relative_blocks$Reference_ID[relative_blocks$node == Art_n] #%>%
+    #filter(node == Art_n) %>% pull(Reference_ID)
     
-    next_group <- target_blocks %>%
-      map_df(~{
+    next_group <- map_df(.x = target_blocks, ~{
       
-        temp <- relative_blocks %>% 
-          filter(Reference_ID %in% .x) 
-        
-        local_origin <- temp %>%
-          filter(node == Art_n) %>% pull(elevation) #find the local origin which is the articulation node n
-          
-        Block_abs_ref <- temp %>% 
-          mutate(elevation = elevation -local_origin + Articulation_df$elevation[1]) #add the global absolute reference
-        
-        return(Block_abs_ref)
-          
-      }
-
+      temp <- relative_blocks[relative_blocks$Reference_ID == .x,] 
+      
+      local_origin <- temp$elevation[temp$node ==Art_n] #find the local origin which is the articulation node n
+      
+      temp$elevation <- temp$elevation -local_origin + Articulation_df$elevation[1] #add the global absolute reference
+      
+      return(temp)
+      
+    }
+    
     )
     
     used_articulation_nodes <- c(used_articulation_nodes, Articulation_df$node[1])
@@ -84,8 +83,7 @@ fix_z_to_origin <- function(relative_blocks, ArticulationVect){
   #The final block is added on here.
   #if I rearrange the process I can get it all in the loop
   #But I just can't be bothered right now :'(
-  absolute_blocks <- next_group %>%
-    bind_rows(absolute_blocks,.) 
+  absolute_blocks <- bind_rows(absolute_blocks,next_group ) 
   
   return(absolute_blocks)
   
